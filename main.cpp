f2aa144d7647aa630b8595c5d04dc9e0e9531c20
Aayush Joglekar <aayushjog@gmail.com>
2025-04-04 08:37:56 +0200

    Improve .gitignore

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 10; i++) {
         std::cout << "i = " << i << std::endl;
     }
 
